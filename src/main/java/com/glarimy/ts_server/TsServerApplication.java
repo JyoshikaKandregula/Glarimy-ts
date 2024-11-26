a24722ef79e6b4336ce95aa7ef4ae48fa8512aa5
JyoshikaKandregula <jyoshika.kandregula@gmail.com>
2024-11-26 08:24:57 +0530

    agging files

---
+++ src/main/java/com/glarimy/ts_server/TsServerApplication.java
@@ -0,0 +1,13 @@
+package com.glarimy.ts_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TsServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TsServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/glarimy/ts_server/TsServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.glarimy.ts_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TsServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
